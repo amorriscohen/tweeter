40b1bcb658e228ac3686e4b1db8f475e7b2ef035
Adam Morris-Cohen <amorriscohen@gmail.com>
2011-10-20 22:01:35 -0500

    Initial commit

---
+++ tweeter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweeter::Application.config.secret_token = '295b7a17f923babda47a5d6f474f8448fc196e834b4ae12d0c0d0a35766e99cee0e4a91fb25ffcc6653a4f882639e69e0ba2cff9a5d129fcbeb8645d5c435a9a'
